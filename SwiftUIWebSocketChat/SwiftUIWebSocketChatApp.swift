ab7fbe7f9f543a3ca477a504b749b8342d12c1a9
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-02-09 18:56:36 +0900

    first commit

---
+++ SwiftUIWebSocketChat/SwiftUIWebSocketChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWebSocketChatApp.swift
+//  SwiftUIWebSocketChat
+//
+//  Created by TokyoYoshida on 2021/02/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWebSocketChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
