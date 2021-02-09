401a2ec7610b334ccd4caf2958cde52356350fbc
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-02-10 05:58:57 +0900

    add type

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
-const http = require("http")
-const server = http.createServer((req, res) => {
+import * as http from "http"
+const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
   res.write('Hello')
   res.end()
 })
