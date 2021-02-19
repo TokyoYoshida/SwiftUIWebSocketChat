f00f8c05a30910fd108a94f733e96f56bc9377d4
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-02-20 05:32:46 +0900

    install express

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,17 @@
-import * as http from "http"
-const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-  res.write('Hello')
-  res.end()
-})
+import express from 'express'
+import * as http from 'http'
+import * as socketio from 'socket.io'
+
+const app: express.Express = express()
+const server: http.Server = http.createServer(app)
+
+app.use(express.static("public"))
+
+// const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+//   res.write('Hello')
+//   res.end()
+// })
+
+// const io = socketio(server)
 
 server.listen(8080)
