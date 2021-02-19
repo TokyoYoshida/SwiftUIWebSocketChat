b07cf5e3aba3b07cb1ee7ff29b841e50694bd25a
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-02-20 05:41:35 +0900

    add server

--- server/server.ts
+++ server/server.ts
@@ -1,17 +1,17 @@
 import express from 'express'
-import * as http from 'http'
-import * as socketio from 'socket.io'
+import http from 'http'
+import socketio from 'socket.io'
 
 const app: express.Express = express()
 const server: http.Server = http.createServer(app)
+const io: socketio.Server = new socketio.Server(server)
 
 app.use(express.static("public"))
 
-// const server = http.createServer((req: http.IncomingMessage, res: http.ServerResponse) => {
-//   res.write('Hello')
-//   res.end()
-// })
-
-// const io = socketio(server)
+io.on('connection', (socket: socketio.Socket) => {
+  socket.on('message', (msg: string) => {
+    io.emit('message', msg)
+  })
+})
 
 server.listen(8080)
