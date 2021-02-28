b531552d82b616670f95c4ef1e6f3bddb8c0dfee
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-03-01 06:29:20 +0900

    add client code

--- js/client.ts
+++ js/client.ts
@@ -1,1 +1,17 @@
-import * as from 'socket.io-client'
+import { format } from 'path'
+import io from 'socket.io-client'
+const socket: SocketIOClient.Socket = io('http://localhost:8080')
+
+const form = document.querySelector('form')
+const input = document.querySelector('input')
+const ul = document.querySelector('ul')
+
+form?.addEventListener('submit', (e: Event) => {
+  e.preventDefault()
+  socket.emit('message', input?.value)
+  input!.value = ''
+})
+
+socket.on('message', (msg: string) => {
+  ul!.insertAdjacentElement('beforebegin', document.createElement(`<li>${msg}</li>`))
+})
