5c5940e0854d62ff820c1e47216854240e18aa34
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-02-28 18:00:44 +0900

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  mode: 'development',
+  entry: './server/server.ts',
+  target: 'node',
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [
+      '.ts', '.js',
+    ],
+  },
+  output: {
+    filename: 'server.js',
+    path: path.join(__dirname, 'dist')
+  }
+};
